4ca066ec29fcd3e177014f652da45e131b5638ce
wrh12345 <wangruiheng0018@163.com>
2024-08-21 07:02:32 +0000

    feature(wrh): add edm initial implementation

--- grl/generative_models/edm_diffusion_model/edm_diffusion_model.py
+++ grl/generative_models/edm_diffusion_model/edm_diffusion_model.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union, Callable
 from dataclasses import dataclass
 
 import numpy as np
@@ -85,14 +85,25 @@ class EDMModel(nn.Module):
         self.sigma_min = INITIAL_SIGMA_MIN[self.edm_type] if "sigma_min" not in self.params else self.params.sigma_min          
         self.sigma_max = INITIAL_SIGMA_MAX[self.edm_type] if "sigma_max" not in self.params else self.params.sigma_max          
 
-            
+    @property       
     def get_type(self) -> str:
         return "EDMModel"
 
     # For VP_edm
     def _sample_sigma_weight_train(self, x: Tensor, **params) -> Tuple[Tensor, Tensor]:
-        # assert the first dim of x is batch size
+        """
+        Overview:
+            Sample sigma from given distribution for training according to edm type.
+            
+        Arguments:
+            x (:obj:`torch.Tensor`): The sample which needs to add noise.
+            
+        Returns:
+            sigma (:obj:`torch.Tensor`): Sampled sigma from the distribution.
+            weight (:obj:`torch.Tensor`): Loss weight obtained from sampled sigma.
+        """
         log.info(f"Params of trainig is: {params}")
+        # assert the first dim of x is batch size
         rand_shape = [x.shape[0]] + [1] * (x.ndim - 1) 
         if self.edm_type == "VP_edm":
             epsilon_t = params.get("epsilon_t", 1e-5)
@@ -116,22 +127,32 @@ class EDMModel(nn.Module):
             weight = (sigma ** 2 + sigma_data ** 2) / (sigma * sigma_data) ** 2
         return sigma, weight
     
-    def forward(self, 
-                x: Tensor, 
-                class_labels: Tensor=None) -> Tensor:
+    def forward(self, x: Tensor, class_labels: Tensor=None) -> Tensor:
         x = x.to(self.device)
         sigma, weight = self._sample_sigma_weight_train(x, **self.params)
         n = torch.randn_like(x) * sigma
         D_xn = self.preconditioner(x+n, sigma, class_labels=class_labels)
         loss = weight * ((D_xn - x) ** 2)
-        return loss
+        return loss.mean()
     
     
-    def _get_sigma_steps_t_steps(self, num_steps: int=18, epsilon_s: float=1e-3, rho: Union[int, float]=7):
+    def _get_sigma_steps_t_steps(self, 
+                                 num_steps: int=18, 
+                                 epsilon_s: float=1e-3, rho: Union[int, float]=7
+                            )-> Tuple[Tensor, Tensor]:
         """
         Overview:
             Get the schedule of sigma according to differernt t schedules.
             
+        Arguments:
+            num_steps (:obj:`int`): The number of timesteps during denoise sampling. Default setting: 18.
+            epsilon_s (:obj:`float`): Parameter epsilon_s (only VP_edm needs).
+            rho (:obj:`Union[int, float]`): Parameter rho (only EDM needs).  
+        
+        Returns:
+            sigma_steps (:obj:`torch.Tensor`): The scheduled sigma.
+            t_steps (:obj:`torch.Tensor`): The scheduled t.
+        
         """
         self.sigma_min = max(self.sigma_min, self.preconditioner.sigma_min)
         self.sigma_max = min(self.sigma_max, self.preconditioner.sigma_max)
@@ -144,11 +165,11 @@ class EDMModel(nn.Module):
             vp_beta_min = np.log(self.sigma_max ** 2 + 1) - 0.5 * vp_beta_d
             
             orig_t_steps = 1 + step_indices / (num_steps - 1) * (epsilon_s - 1)
-            sigma_steps = SIGMA_T["VP_edm"](orig_t_steps, vp_beta_d, vp_beta_min)
+            sigma_steps = SIGMA_T[self.edm_type](orig_t_steps, vp_beta_d, vp_beta_min)
         
         elif self.edm_type == "VE_edm":
             orig_t_steps = (self.sigma_max ** 2) * ((self.sigma_min ** 2 / self.sigma_max ** 2) ** (step_indices / (num_steps - 1)))
-            sigma_steps = SIGMA_T["VE_edm"](orig_t_steps)
+            sigma_steps = SIGMA_T[self.edm_type](orig_t_steps)
         
         elif self.edm_type == "iDDPM_edm":
             M, C_1, C_2 = self.params.M, self.params.C_1, self.params.C_2
@@ -166,19 +187,27 @@ class EDMModel(nn.Module):
             sigma_steps = (self.sigma_max ** (1 / rho) + step_indices / (num_steps - 1) * \
                 (self.sigma_min ** (1 / rho) - self.sigma_max ** (1 / rho))) ** rho
             orig_t_steps = SIGMA_T_INV[self.edm_type](self.preconditioner.round_sigma(sigma_steps))         
+        else:
+            raise NotImplementedError(f"Please check your edm_type: {self.edm_type}, which is not in ['VP_edm', 'VE_edm', 'iDDPM_edm', 'EDM']")
         
-        t_steps = torch.cat([orig_t_steps, torch.zeros_like(orig_t_steps[:1])]) # t_N = 0
+        t_steps = torch.cat([orig_t_steps, torch.zeros_like(orig_t_steps[:1])]) # t_N = 0E
         
         return sigma_steps, t_steps  
     
     
-    def _get_sigma_deriv_inv_scale_deriv(self, epsilon_s=1e-3):
+    def _get_sigma_deriv_inv_scale_deriv(self, epsilon_s: Union[int, float]=1e-3) \
+                                -> Tuple[Callable, Callable, Callable, Callable, Callable]:
         """
         Overview:
             Get sigma(t) for different solver schedules.
             
         Returns:
-            sigma(t), sigma'(t), sigma^{-1}(sigma) 
+            sigma: (:obj:`Callable`): sigma(t)
+            sigma_deriv: (:obj:`Callable`): sigma'(t)
+            sigma_inv: (:obj:`Callable`): sigma^{-1} (sigma) 
+            scale: (:obj:`Callable`): s(t)
+            scale_deriv: (:obj:`Callable`): s'(t)
+            
         """
         vp_beta_d = 2 * (np.log(self.sigma_min ** 2 + 1) / epsilon_s - np.log(self.sigma_max ** 2 + 1)) / (epsilon_s - 1)
         vp_beta_min = np.log(self.sigma_max ** 2 + 1) - 0.5 * vp_beta_d
