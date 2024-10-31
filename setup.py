e75042a65215c6e1a9f5992e49e2f7c94ebe9f6a
zjowowen <zjowowen@outlook.com>
2024-10-31 18:31:11 +0800

    Polish setup.py.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description=readme,
     long_description_content_type='text/markdown',
     author='OpenDILab',
-    author_email="zjowowen@gmail.com",
+    author_email="opendilab@pjlab.org.cn",
     url="https://github.com/opendilab/GenerativeRL",
 
     packages=find_packages(
